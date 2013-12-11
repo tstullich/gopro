92b402e66cfeb25791da665ea1b6abf76650d25e
tstullich <timstullich@gmail.com>
2013-12-11 09:25:56 -0800

    Added the go file for the Red-Black Tree.

---
+++ src/ds/rbt/rbt.go
@@ -0,0 +1,14 @@
+package rbt
+
+/*
+ * This will be the file for the Red Black Tree.
+ *
+ */
+
+type RedBlackTree struct {
+
+}
+
+func NewRedBlackTree() *RedBlackTree {
+    return new(RedBlackTree)
+} 
